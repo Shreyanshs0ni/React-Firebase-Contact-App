790e214368186d37a1fa1b486ca2587084f2e52f
Shreyanshs0ni <shreyanshsoni07@gmail.com>
2024-11-29 20:19:04 +0530

    search bar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import Navbar from "./components/Navbar";
+import { FaSearch } from "react-icons/fa";
+import { IoIosAddCircleOutline } from "react-icons/io";
 
 const App = () => {
   return (
-    <div>
+    <div className="max-w-[370px mx-auto px-4">
       <Navbar />{" "}
+      <div className="flex gap-2">
+        <div className="relative flex flex-grow items-center">
+          <FaSearch className="absolute ml-1 text-xl text-white" />
+          <input
+            type="text"
+            placeholder="Search Contact"
+            className="h-8 flex-grow rounded-md border border-[orange] bg-transparent pl-8 text-white"
+          />
+        </div>
+        <IoIosAddCircleOutline className="cursor-pointer text-4xl text-white" />
+      </div>
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import Logo from "../assets/Logo.png";
 
 const Navbar = () => {
   return (
-    <div className="m-4 flex h-[60px] items-center justify-center gap-2 rounded-lg bg-[orange]">
+    <div className="my-4 flex h-[60px] items-center justify-center gap-2 rounded-lg bg-[orange] text-xl font-medium">
       <img className="h-[30px] w-[30px]" src={Logo} alt="logo" />
       <h1>Contact App</h1>
     </div>
